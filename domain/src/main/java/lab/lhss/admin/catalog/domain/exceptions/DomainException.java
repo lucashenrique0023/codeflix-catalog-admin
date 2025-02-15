bde6f5dc3a30eaa979ff009af4e284eedb402230
Lucas Henrique <lucas.henrique0023@gmail.com>
2025-02-14 21:49:48 -0300

    Removing Stacktrace of DomainException
    Removing stacktrace to increase app performance, no need for stacktrace.

--- domain/src/main/java/lab/lhss/admin/catalog/domain/exceptions/DomainException.java
+++ domain/src/main/java/lab/lhss/admin/catalog/domain/exceptions/DomainException.java
@@ -4,17 +4,21 @@ import lab.lhss.admin.catalog.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStacktraceException {
 
     private final List<Error> errors;
 
-    private DomainException(List<Error> errors) {
-        super("", null, true, false);
+    private DomainException(final String message, List<Error> errors) {
+        super(message);
         this.errors = errors;
     }
 
-    public static DomainException with(final List<Error> errors) {
-        return new DomainException(errors);
+    public static DomainException with(final Error error) {
+        return new DomainException(error.message(), List.of(error));
+    }
+
+    public static DomainException with(final List<Error> error) {
+        return new DomainException("", error);
     }
 
     public List<Error> getErrors() {
