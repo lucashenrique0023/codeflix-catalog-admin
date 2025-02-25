4c3478ec950449b8d6021a883d3172d2dc4cd2ca
Lucas Henrique <lucas.henrique0023@gmail.com>
2025-02-25 17:11:13 -0300

    Configuring application test datasource

--- infrastructure/src/test/java/lab/lhss/MainTest.java
+++ infrastructure/src/test/java/lab/lhss/MainTest.java
@@ -3,10 +3,12 @@ package lab.lhss;
 import lab.lhss.admin.catalog.infrastructure.Main;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
 
 class MainTest {
     @Test
     void testMain() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         Assertions.assertNotNull(new Main());
         Main.main(new String[]{});
     }
